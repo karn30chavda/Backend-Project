bee742173e6ce0fbd76f10ba815de08533d6018b
karn30chavda <karanchavda543@gmail.com>
2025-08-09 17:09:09 +0530

    add multer middleware for file uploads and cloudinary utility for file storage

---
+++ src/middlewares/multer.middlewares.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// Middleware for handling file uploads using multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/public/temp");
+  },
+  filename: function (req, file, cb) {
+    // Use the original file name for the uploaded file
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configure Cloudinary with environment variables
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Function to upload a file to Cloudinary with error handling and local file cleanup
+const cloudinaryFileUploader = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+    console.log(`File uploaded Successfully in cloudinary`, response.url);
+    return response;
+  } catch (error) {
+    console.error("Error uploading file to Cloudinary:", error);
+    // Handle the error appropriately if upload fails in Cloudinary, fs will delete the local file from the server to avoid memory leaks, corrupt files, etc.
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { cloudinaryFileUploader };
