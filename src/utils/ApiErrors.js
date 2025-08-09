a3e8f621fcef99fb048a579b8b92a124324b9fe6
karn30chavda <karanchavda543@gmail.com>
2025-08-09 16:03:49 +0530

    common error resolve

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -8,8 +8,8 @@ class ApiError extends Error {
     this.data = null;
     this.success = false;
 
-    if (statck) {
-      this.statck = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
